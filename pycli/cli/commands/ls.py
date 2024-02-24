e2ea3546b776b35d9b4f9f624dc5ffa5a731b226
Alexey Radkevich <alexey.radkevich@productmadness.com>
2024-02-24 20:14:18 +0100

    refactor(commands): use annotated args, moved logic to separate function

--- pycli/cli/commands/ls.py
+++ pycli/cli/commands/ls.py
@@ -1,4 +1,5 @@
 import typer
+from typing_extensions import Annotated
 from pathlib import Path
 from rich.console import Console
 from rich.columns import Columns
@@ -11,16 +12,20 @@ WITHOUT_HIDDEN_GLOB_PATTERN = "[!.]*"
 
 
 def handle(
-    path: str = typer.Argument("."),
-    show_hidden: bool = typer.Option(False)
+    path: Annotated[str, typer.Argument()] = ".",
+    show_hidden: Annotated[bool, typer.Option()] = False
 ):
+    files = __files_list__(path, show_hidden)
+    columns = Columns(files, equal=True, expand=True)
+    Console().print(columns)
+
+
+def __files_list__(path: str, show_hidden: bool):
     glob_pattern = (
         WITH_HIDDEN_GLOB_PATTERN if show_hidden
         else WITHOUT_HIDDEN_GLOB_PATTERN
     )
-    files = [__format__(item) for item in Path(path).glob(glob_pattern)]
-    columns = Columns(files, equal=True, expand=True)
-    Console().print(columns)
+    return [__format__(item) for item in Path(path).glob(glob_pattern)]
 
 
 def __format__(file: Path):
--- pycli/cli/commands/mkdir.py
+++ pycli/cli/commands/mkdir.py
@@ -1,7 +1,9 @@
 import os
+import typer
+from typing_extensions import Annotated
 
 
 def handle(
-    path: str
+    path: Annotated[str, typer.Argument()]
 ):
     os.makedirs(path)
