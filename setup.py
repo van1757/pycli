8595bfd9d9ec9c1dc0682b9eced02be9af07c603
Alexey Radkevich <alexey.radkevich@productmadness.com>
2024-02-25 23:09:26 +0100

    chore(app): add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.0.1"
+__description__ = "Python-written clone of Unix commands"
+
+setup(
+    name="pycli_learning",
+    version=__version__,
+    author="Alexey Radkevich",
+    description=__description__,
+    packages=find_packages(),
+    keywords=["python"]
+)
